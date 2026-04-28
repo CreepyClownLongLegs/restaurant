83a21561748d6cd1e78133c4e5553549acc07de6
CreepyClownlongLegs <majda.horozovic@hotmail.com>
2026-04-28 22:39:20 +0200

    Added contacts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Reservation } from './reservation/reservation';
 import { Menu } from './menu/menu';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
     {path: '', component: Home, title: 'Your Restaurant'},
     {path: 'reservation', component: Reservation, title: 'reserve a table'},
-    {path: 'menu', component: Menu, title: 'Your Menu'}
+    {path: 'menu', component: Menu, title: 'Your Menu'},
+    {path: 'contact', component: Contact, title: 'Your Contact'}
 ];
